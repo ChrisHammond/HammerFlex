a2ff040ead155d54fc20027c89dfa28b77e1838b
ChrisHammond <github@christoc.com>
2014-11-28 23:56:16 -0600

    Changes for Bootstrap 3.3.1, prepping for v00.01.08 release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("00.01.07.*")]
-[assembly: AssemblyFileVersion("00.01.07.*")]
+[assembly: AssemblyVersion("00.01.08.*")]
+[assembly: AssemblyFileVersion("00.01.08.*")]
