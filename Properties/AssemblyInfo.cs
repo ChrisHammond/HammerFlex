468e3a600aea44065d18d2f51b527d4269e7b1d4
Chris Hammond <github@christoc.com>
2015-09-08 22:54:27 -0500

    Updates for V00.01.13

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("00.01.12.*")]
-[assembly: AssemblyFileVersion("00.01.12.*")]
+[assembly: AssemblyVersion("00.01.13.*")]
+[assembly: AssemblyFileVersion("00.01.13.*")]
